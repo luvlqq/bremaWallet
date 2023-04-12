3122bf2fcd54a2f67b09d6b64555a7ebefb47d50
luvlqq <lualgod92@gmail.com>
2023-04-12 18:24:53 +0300

    rewrite in future

--- packages/server/src/user/user.controller.ts
+++ packages/server/src/user/user.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Get, HttpStatus, Param } from '@nestjs/common';
+import {Controller, Get, HttpStatus, Param, Req, UseGuards} from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserDto } from './dto/user.dto';
+import { JwtAuthGuard } from '../auth/jwt.guard';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get(':id')
+  @Get()
   @ApiOperation({ summary: 'get user balance' })
   @ApiParam({
     name: 'Balance data',
@@ -17,7 +18,13 @@ export class UserController {
   })
   @ApiResponse({ status: HttpStatus.OK, description: 'Success', type: UserDto })
   @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad Request' })
-  async getUsersBalance(@Param('id') dto: UserDto) {
+  async getUsersBalance(@Param() dto: UserDto) {
     return this.userService.getBalance(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  getMyUser(@Param() params: { id: number }, @Req() req) {
+    return this.userService.getMyUser(params.id, req);
   }
 }
--- packages/server/src/user/user.service.ts
+++ packages/server/src/user/user.service.ts
@@ -1,16 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { UserDto } from './dto/user.dto';
+import { Request } from 'express';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
   async getBalance(dto: UserDto) {
-    const { id, balance } = dto;
+    const { id, balance, login } = dto;
     return this.prisma.user.findMany({
-      select: { balance: true },
+      select: { balance: true, login: true },
       where: { id },
     });
+  }
+
+  async getMyUser(id: number, req: Request) {
+    const user = this.prisma.user.findUnique({ where: { id } });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return { user };
   }
 }
