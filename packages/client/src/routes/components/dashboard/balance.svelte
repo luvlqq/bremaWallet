2d294093cbfbf764b243277249192e0aba53453b
luvlqq <lualgod92@gmail.com>
2023-04-12 23:02:40 +0300

    model window button fix. Fix buttons position in model window

--- packages/client/src/routes/components/dashboard/balance.svelte
+++ packages/client/src/routes/components/dashboard/balance.svelte
@@ -17,11 +17,14 @@
         <div class="fixed top-0 left-0 w-full h-full flex justify-center items-center z-20">
             <div class="bg-white p-8 rounded-lg">
                 <h2 class="text-lg font-medium mb-4 text-black">Money Send</h2>
-                <input type="text" class="rounded-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="User login">
-                <input type="number" min="1" class="my-4 rounded-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Amount">
-                <button type="button" class="my-4 mx-4 bg-purple-800 text-white px-4 py-2 rounded-lg hover:bg-purple-700 focus:outline-none" on:click={toggleModal}>Close</button>
-                <button type="button" class="my-4 mx-4 bg-purple-800 text-white px-4 py-2 rounded-lg hover:bg-purple-700 focus:outline-none" on:click={toggleModal}>Send</button>
+                <input type="text" class="rounded-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="User login">
+                <input type="number" min="1" class="my-4 rounded-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Amount">
+                <div class="flex justify-center">
+                    <button type="button" class="my-4 mr-5 px-4 bg-purple-800 text-white px-4 py-2 rounded-lg hover:bg-purple-700 focus:outline-none" on:click={toggleModal}>Close</button>
+                    <button type="button" class="my-4 ml-5 px-4 bg-purple-800 text-white px-4 py-2 rounded-lg hover:bg-purple-700 focus:outline-none" on:click={toggleModal}>Send</button>
+                </div>
             </div>
+
         </div>
     {/if}
 </div>
